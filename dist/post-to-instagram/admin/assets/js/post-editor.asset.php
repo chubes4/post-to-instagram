9d302f28f7e5949c6cdf1be44c439137d1c04f61
“Chris <chubes@extrachill.com>
2025-06-05 11:12:43 -0500

    Remove .cursor, .DS_Store, and dist from repo and update .gitignore

--- dist/post-to-instagram/admin/assets/js/post-editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins'), 'version' => '405e30e817156a593ff7');
